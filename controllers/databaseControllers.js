658ed405d7539b120c934b6da233332706595072
farizrivaldo98 <123930917+farizrivaldo98@users.noreply.github.com>
2023-04-15 16:20:51 +0700

    create product & Categories (BE)

--- controllers/databaseControllers.js
+++ controllers/databaseControllers.js
@@ -16,4 +16,40 @@ module.exports = {
       return response.status(200).send(result);
     });
   },
+  addProduct: async (request, response) => {
+    const { name, price, description, id_users, id_categories } = request.body;
+    let fetchQuerry = `INSERT INTO products VALUES (null, ${db.escape(
+      name
+    )}, ${db.escape(price)}, ${db.escape(description)}, ${db.escape(
+      id_users
+    )}, ${db.escape(id_categories)})`;
+    db.query(fetchQuerry, (err, result) => {
+      if (err) {
+        return response.status(400).send(err.message);
+      } else {
+        let fatchquerry = "SELECT * FROM products";
+        db.query(fatchquerry, (err, result) => {
+          return response.status(200).send(result);
+        });
+      }
+    });
+  },
+
+  addCategories: async (request, response) => {
+    const { name, id_users } = request.body;
+
+    let fetchQuerry = `INSERT INTO categories VALUES (null, ${db.escape(
+      name
+    )},${db.escape(id_users)})`;
+    db.query(fetchQuerry, (err, result) => {
+      if (err) {
+        return response.status(400).send(err.message);
+      } else {
+        let fatchquerry = "SELECT * FROM categories";
+        db.query(fatchquerry, (err, result) => {
+          return response.status(200).send(result);
+        });
+      }
+    });
+  },
 };
